671a28b674f1f3365607dd5bd46d5f597e3d9faf
Ken Finnigan <ken@kenfinnigan.me>
2016-07-01 22:34:33 -0400

    Add some layout to page and add sub page classes

--- chapter2/ui/src/main/resources/js/app.js
+++ chapter2/ui/src/main/resources/js/app.js
@@ -1,13 +1,42 @@
 var Router         = ReactRouter;
-var RouteHandler  = Router.RouteHandler;
-var DefaultRoute     = ReactRouter.DefaultRoute;
+var RouteHandler   = Router.RouteHandler;
+var DefaultRoute   = ReactRouter.DefaultRoute;
 var Route          = Router.Route;
+var Link           = Router.Link;
 
 var App = React.createClass({
   render: function() {
     return (
       <div>
-        <RouteHandler/>
+        <LeftNav/>
+        <div className="container-fluid container-cards-pf container-pf-nav-pf-vertical container-pf-nav-pf-vertical-with-secondary ">
+          <div className="row row-cards-pf">
+            <RouteHandler/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+var LeftNav = React.createClass({
+  render: function() {
+    return (
+      <div className="nav-pf-vertical nav-pf-vertical-with-secondary-nav ">
+        <ul className="list-group">
+          <li className="list-group-item active">
+            <Link to="employees">
+              <span className="pficon pficon-users"></span>
+              <span className="list-group-item-value">Employees</span>
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link to="addresses">
+              <span className="fa fa-book"></span>
+              <span className="list-group-item-value">Addresses</span>
+            </Link>
+          </li>
+        </ul>
       </div>
     );
   }
@@ -16,11 +45,39 @@ var App = React.createClass({
 var Home = React.createClass({
   render: function() {
     return (
-      <div id="Welcome">
-        <p>
-        Employees Application
-        </p>
-      </div>
+      <Employees/>
+    );
+  }
+});
+
+var Employees = React.createClass({
+  render: function() {
+    return (
+      <h1>Employees</h1>
+    );
+  }
+});
+
+var Employee = React.createClass({
+  render: function() {
+    return (
+      <h1>Single Employee</h1>
+    );
+  }
+});
+
+var Addresses = React.createClass({
+  render: function() {
+    return (
+      <h1>Addresses</h1>
+    );
+  }
+});
+
+var Address = React.createClass({
+  render: function() {
+    return (
+      <h1>Single Address</h1>
     );
   }
 });
@@ -28,6 +85,14 @@ var Home = React.createClass({
 var routes = (
   <Route path="/" handler={App}>
     <DefaultRoute name="home" handler={Home}/>
+    <Route name="employees">
+      <Route name="employee" path=":id" handler={Employee}/>
+      <DefaultRoute handler={Employees}/>
+    </Route>
+    <Route name="addresses">
+      <Route name="address" path=":id" handler={Address}/>
+      <DefaultRoute handler={Addresses}/>
+    </Route>
   </Route>
 );
 
